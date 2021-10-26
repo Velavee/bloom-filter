a7e61ebf61746a97b72a9a1836705d908145d2ba
Velavee <59717974+Velavee@users.noreply.github.com>
2021-10-25 17:19:02 -0700

    feat: create map of password hashes

--- main.py
+++ main.py
@@ -18,16 +18,28 @@ def create_blake3_hash(word):
     hash_setup.update(bytes_word)
     return hash_setup
 
+def add_to_map(a_map, sha1_hash, md5_hash, blake2b_hash):
+    a_map[sha1_hash] = 1
+    a_map[md5_hash] =  1
+    a_map[blake2b_hash] = 1
+
+
 def create_map(dictionary):
+    password_dict = {}
+
     for line in dictionary:
         sha1_hash = create_sha1_hash(line)
         md5_hash = create_md5_hash(line)
         blake2b_hash = create_blake3_hash(line)
 
+        add_to_map(password_dict, sha1_hash, md5_hash, blake2b_hash)
+
+    return password_dict
+
 
 def main():
     dict = open('dictionary.txt', 'r')
-    create_map(dict)
+    password_dict = create_map(dict)
 
 if __name__ == '__main__':
     main()
